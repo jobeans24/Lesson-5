04674d23d120342a22d8ee39e704b6e76e9c625b
Josie Zobel <159351625+jobeans24@users.noreply.github.com>
2024-03-14 14:19:17 -0400

    03/12

--- 08-Stu_Dom-Traversal/Unsolved/assets/js/script.js
+++ 08-Stu_Dom-Traversal/Unsolved/assets/js/script.js
@@ -1,6 +1,10 @@
 // Assemble: Create/select DOM elements
-var rootEl = $('#root');
+let rootEl = $('#root');
 
 // TODO: Starting from the root element, `rootEl`, make all the background color for each box white.
+rootEl.find('.box').css('background-color', 'white');
+
 
 // TODO: Starting from `rootEl`, create the jQuery statement that will add "O" to block the "X" from winning
+rootEl.find('.box').eq(0).text('O');
+
--- 10-Stu_Event-Delegation/Unsolved/assets/js/script.js
+++ 10-Stu_Event-Delegation/Unsolved/assets/js/script.js
@@ -29,6 +29,13 @@ function handleFormSubmit(event) {
 }
 
 // TODO: Create a function to handle removing a list item when `.delete-item-btn` is clicked
+function handleRemoveItemClick(event) {
+  var listItem = $(event.target).closest('li');
+  listItem.remove();
+}
+
+// TODO: Use event delegation and add an event listener to `shoppingListEl` to listen for a click event on any element with a class of `.delete-item-btn` and execute the function created above
+shoppingListEl.on('click', '.delete-item-btn', handleRemoveItemClick);
 
 // TODO: Use event delegation and add an event listener to `shoppingListEl` to listen for a click event on any element with a class of `.delete-item-btn` and execute the function created above
 
